a3df147f1021864cdd6088973f51169dd37a508a
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-07-28 23:49:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const append = ["Milo", "Otis", "Garfield"]
 const prepend = ["Milo", "Otis", "Garfield"]
 const removeLast = ["Milo", "Otis", "Garfield"]
 const removeFirst = ["Milo", "Otis", "Garfield"]
+
+append.push('Odie'); 
+prepend.unshift('Oliver');
+removeLast.pop();
+removeFirst.shift();
